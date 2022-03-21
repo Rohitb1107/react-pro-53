d6e453d88b855288424587ae659f0a74d165bc7a
Rohit <95957671+Rohitb1107@users.noreply.github.com>
2022-03-21 14:35:39 +0530

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import { AllRoutes } from "./Routes/Routes";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <AllRoutes /> */}
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
